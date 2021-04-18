d1e0db531eb31e1897f57e3022e2378d5eb3e97c
Breno Battaglin <breno.battaglin@icloud.com>
2021-04-18 12:00:48 -0300

    Add uninstall built-in apps script

---
+++ setup/uninstall.ps1
@@ -0,0 +1,61 @@
+## Uninstall buil-in apps
+
+## Uninstall 3D Builder:
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+
+## Uninstall Calendar and Mail:
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+
+## Uninstall Get Office:
+Get-AppxPackage *officehub* | Remove-AppxPackage
+
+## Uninstall Get Skype:
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+
+## Uninstall Get Started:
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+
+## Uninstall Get Help:
+Get-AppxPackage *gethelp* | Remove-AppxPackage
+
+## Uninstall Groove Music:
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+
+## Uninstall Microsoft Solitaire Collection:
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+
+## Uninstall Money:
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+
+## Uninstall Movies & TV:
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+
+## Uninstall News:
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+
+## Uninstall OneNote:
+Get-AppxPackage *onenote* | Remove-AppxPackage
+
+## Uninstall People:
+Get-AppxPackage *Microsoft.people* | Remove-AppxPackage
+
+## Uninstall Phone Companion:
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+
+## Uninstall Your Phone
+Get-AppxPackage yourphone* | Remove-AppxPackage
+
+## Uninstall Photos:
+Get-AppxPackage *photos* | Remove-AppxPackage
+
+## Uninstall Sports:
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+
+## Uninstall Weather:
+Get-AppxPackage *bingweather* | Remove-AppxPackage
+
+## Uninstall FeedbackHub:
+Get-AppxPackage feedbackhub| Remove-AppxPackage
+
+## Uninstall Maps:
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
