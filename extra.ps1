f57992e166f0a4adeec909f367da424a777e4418
Breno <breno.battaglin@icloud.com>
2021-04-20 14:13:44 -0300

    Fix extra script
    I accidentally committed my personal info.

--- extra.ps1
+++ extra.ps1
@@ -1,8 +1,8 @@
 # Git credentials
 # Not in the repository, to prevent people from accidentally committing under my name
 
-$env:GitAuthorName = 'Breno Battaglin'
-$env:GitAuthorEmail = 'breno.battaglin@icloud.com'
+$env:GitAuthorName = 'first and last name'
+$env:GitAuthorEmail = 'your email'
 $env:GitSshDirectory = Write-Host "$env:USERPROFILE\.ssh\id_rsa"
 
 git config --global user.name $env:GitAuthorName
