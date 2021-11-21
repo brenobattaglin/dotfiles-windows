405e02316ef15fcc489ce96c4a8ca86f3deaf06c
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-21 14:49:26 -0300

    Create Chocolatey scripts

---
+++ chocolatey/setup.ps1
@@ -0,0 +1,26 @@
+#Requires -RunAsAdministrator
+
+## Install Chocolatey packages
+# Productivity
+cinst authy-desktop `
+    freefilesync `
+    powertoys `
+    qbittorrent `
+    gimp `
+    gopro-quik `
+    mpc-be `
+    spotify `
+    docker-desktop `
+    docker-compose `
+    flutter `
+    git `
+    vscode
+    battle.net `
+    discord `
+    epicgameslauncher `
+    logitechgaming `
+    steam `
+    bleachbit `
+    malwarebytes `
+    rufus `
+    lghub
