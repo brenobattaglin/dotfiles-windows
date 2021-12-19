8fbc24f8ff6105b54a62e7e64305a20987c9155a
Breno Battaglin <breno.battaglin@icloud.com>
2021-12-19 20:39:04 -0300

    Remove gopro quik choco pkg

--- chocolatey/setup.ps1
+++ chocolatey/setup.ps1
@@ -2,24 +2,24 @@
 
 ## Install Chocolatey packages
 # Productivity
-cinst authy-desktop `     
-    battle.net `
-    bleachbit `
-    discord `
-    docker-compose `
-    docker-desktop `
-    epicgameslauncher `
-    flutter `
-    freefilesync `
-    gimp `
-    git `
-    gopro-quik `
-    logitechgaming `
-    malwarebytes `
-    mpc-be `
-    powertoys `
-    qbittorrent `
-    rufus 
-    spotify `
-    steam `
-    vscode
+cinst amazongames 
+authy-desktop `     
+battle.net `
+bleachbit `
+discord `
+docker-compose `
+docker-desktop `
+epicgameslauncher `
+flutter `
+freefilesync `
+gimp `
+git `
+logitechgaming `
+malwarebytes `
+mpc-be `
+powertoys `
+qbittorrent `
+rufus 
+spotify `
+steam `
+vscode
