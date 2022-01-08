af995990d620abb55e1c9bc3e337c1343ac52e90
Breno Battaglin <breno.battaglin@icloud.com>
2022-01-08 12:32:08 -0300

    Create .gitconfig force copy command

--- git/setup.ps1
+++ git/setup.ps1
@@ -1,9 +1,15 @@
+# Find the .gitconfig file path. This prevents directory error case  
+# this script is executed from the root dir or its current dir.
+$gitconfigPath = Get-ChildItem -Recurse | Where {$_.Name -match '.gitconfig'}
+
 # Git credentials not in the repository, to prevent people from accidentally committing under my name
+$gitAuthorName = 'First and Last Name'
+$gitAuthorEmail = 'Email address'
 
-$env:GitAuthorName = 'your name and last name'
-$env:GitAuthorEmail = 'your email here'
+# Copy .gitconfig file to the current user's home directory
+Copy-Item $gitconfigPath.FullName -Destination "$env:USERPROFILE\" -Force -Verbose 
 
-git config --global user.name $env:GitAuthorName
-git config --global user.email $env:GitAuthorEmail
+git config --global user.name $gitAuthorName
+git config --global user.email $gitAuthorEmail
 
-ssh-keygen -t ed25519 -C $env:GitAuthorEmail
+ssh-keygen -t ed25519 -C $gitAuthorEmail
