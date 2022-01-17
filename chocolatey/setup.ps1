c91f77136ca5ee486e9c5e06f63e70a2086749f9
Breno Battaglin <breno.battaglin@icloud.com>
2022-01-17 19:25:34 -0300

    Refactor chocolatey setup script
    - create package array and for each loop

--- chocolatey/setup.ps1
+++ chocolatey/setup.ps1
@@ -1,28 +1,31 @@
 #Requires -RunAsAdministrator
 
+$packages = @(
+ 'amazongames' 
+ 'authy-desktop'
+ 'battle.net'
+ 'bleachbit'
+ 'discord'
+ 'docker-compose'
+ 'docker-desktop'
+ 'epicgameslauncher'
+ 'firefox'
+ 'flutter'
+ 'freefilesync'
+ 'gimp'
+ 'goggalaxy'
+ 'git'
+ 'logitechgaming'
+ 'malwarebytes'
+ 'mpc-be'
+ 'nodejs'
+ 'origin'
+ 'powertoys'
+ 'qbittorrent'
+ 'rufu'
+ 'spotify'
+ 'steam'
+)
+
 ## Install Chocolatey packages
-# Productivity
-cinst amazongames 
-authy-desktop `     
-battle.net `
-bleachbit `
-discord `
-docker-compose `
-docker-desktop `
-epicgameslauncher `
-flutter `
-freefilesync `
-gimp `
-goggalaxy `
-git `
-logitechgaming `
-malwarebytes `
-mpc-be `
-nodejs `
-origin `
-powertoys `
-qbittorrent `
-rufus 
-spotify `
-steam `
-vscode
+foreach ($pkg in $packages) { choco install $pkg}
